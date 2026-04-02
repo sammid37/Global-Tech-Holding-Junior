5aaefe61a0b44bafaf52b6dc17e5f03d6284cd67
sammid37 <samanthadmedeiros37@gmail.com>
2026-04-02 20:37:12 -0300

    backend tests

---
+++ src/test/java/com/sammid37/health_person/HealthPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sammid37.health_person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
